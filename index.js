4a698ada417b633bc85fe584758533439780498b
Elena Aseeva <aseeva.e.s@gmail.com>
2023-03-27 15:36:59 +0300

    3.8*: Phonebook backend step8 done

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('params', function(req, res) {
+    return JSON.stringify(req.body);
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :params'));
+
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
